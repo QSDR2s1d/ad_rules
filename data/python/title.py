65d7cf98e649d18d3a28a7211cbf5b8040afb489
QSDR2s1d <123747570+QSDR2s1d@users.noreply.github.com>
2025-10-04 21:38:56 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -19,9 +19,9 @@ for file_path in file_list:
     line_count = content.count('\n') + 1
 
     # 在文件顶部插入内容
-    new_content = f"[个人合并 2.0]\n" \
-                  f"! Title: 去广告规则，酷安反馈反馈\n" \
-                  f"! Homepage: https://github.com/qq5460168/666\n" \
+    new_content = f"[forked from qq5460168/666]\n" \
+                  f"! Title: ad_rules\n" \
+                  f"! Homepage: https://github.com/QSDR2s1d/ad_rules\n" \
                   f"! Expires: 12 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
                   f"! Description: 适用于AdGuard的去广告规则，合并优质上游规则并去重整理排列\n" \
